c8db083802cab8962c57a8c4ce381414d002c7f1
jonathanMcShane <jonathan.mcshane@yahoo.com>
2024-08-29 13:38:52 +0100

    Added crawlPage function - fetches and parses the html of the given URL

--- crawl.js
+++ crawl.js
@@ -1,4 +1,7 @@
+import { JSDOM } from 'jsdom'
+
 function normalizeURL(url) {
+
     const urlObj = new URL(url)
     let fullPath = `${urlObj.host}${urlObj.pathname}`
     if (fullPath.slice(-1) === '/') {
@@ -7,4 +10,54 @@ function normalizeURL(url) {
     return fullPath
 }
 
-export { normalizeURL };
+function getURLsFromHTML(html, baseURL) {
+
+    const urls = []
+    const dom = new JSDOM(html)
+    const anchors = dom.window.document.querySelectorAll('a')
+    for (const anchor of anchors) {
+        if (anchor.hasAttribute('href')) {
+            let href = anchor.getAttribute('href')
+
+            try {
+                // convert any relative URLs to absolute URLs
+                href = new URL(href, baseURL).href
+                urls.push(href)
+            } catch (err) {
+                console.log(`${err.message}: ${href}`)
+            }
+        }
+    }
+    return urls
+}
+
+async function crawlPage(currentUrl) {
+    // Fetch and parse the html of the currentUrl
+    console.log(`Crawling ${currentUrl}`)
+
+    let response
+
+    try {
+        response = await fetch(currentUrl)
+    } catch (err) {
+        throw new Error(`Network error: ${err.message}`)
+    }
+
+    if (response.status >= 400) {
+        console.log(`Error Code: ${response.status} ${response.statusText}`)
+        return
+    }
+
+    const contentType = response.headers.get('content-type')
+    if (!contentType || !contentType.startsWith('text/html')) { // If the content-type header does not start with text/html, log an error
+        console.log(`Error - Got non-HTML response: ${contentType}`)
+        return
+    }
+
+    // Read and log the HTML content properly
+    const htmlContent = await response.text()  // Await the response text here
+    console.log(htmlContent)  // This will log the HTML content
+
+}
+
+export { normalizeURL, getURLsFromHTML, crawlPage };
--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log('Hello world')
+import { argv } from 'node:process';
+import { crawlPage } from './crawl.js'
+
+async function main() {
+    const argument = argv[2]
+    if (argv.length < 3) {
+        console.log('No website provided...')
+        return
+    }
+
+    if (argv.length > 3) {
+        console.log('Too many arguments provided...')
+        return
+    }
+
+    const baseUrl = argv[2]
+
+    console.log(`Starting crawl of ${baseUrl}`)
+
+    crawlPage(argument)
+}
+
+main()
